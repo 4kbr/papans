ee671a3c512965d3389b60a805e08e9099a217ed
4kbr <igyutubfesbuk@gmail.com>
2023-11-28 15:56:45 +0700

    start

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="text-sky-300 text-4xl">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque,
+      blanditiis.
+    </div>
+  );
+};
+
+export default Home;
